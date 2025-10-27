6deac58f777eaaef741bbb3107541422d9ab0a2c
Barbara Gocłowska <gocbar@gmail.com>
2025-10-27 19:59:50 +0100

    Zaczynamy

--- src/main/java/pl/gocbar/gallery/HomeController.java
+++ src/main/java/pl/gocbar/gallery/HomeController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class HomeController {
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
+	// http://localhost:8080/hello-world
+	@GetMapping("/hello-world")
+	public String home(Model model) {
+		model.addAttribute("message", "Hello world");
+		return "hello-world";
 	}
 }
