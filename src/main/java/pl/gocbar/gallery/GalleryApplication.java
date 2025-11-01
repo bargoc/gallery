c479ea51050c0bd6c4ceb9bc67775c3360ea5a4f
Barbara Gocłowska <gocbar@gmail.com>
2025-11-01 11:01:09 +0100

    Poprawne odwzorowanie kalsy na tabelę

--- src/main/java/pl/gocbar/gallery/GalleryApplication.java
+++ src/main/java/pl/gocbar/gallery/GalleryApplication.java
@@ -9,13 +9,13 @@ public class GalleryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GalleryApplication.class, args);
 
-		Student student = new Student();
-		student.setId(1);
-		student.setName("Ambrozy");
-		student.setEmail("ambrozy@gmail.com");
-		System.out.println(student.getId());
-		System.out.println(student.getName());
-		System.out.println(student.getEmail());
+		// Student student = new Student();
+		// student.setId(1);
+		// student.setName("Ambrozy");
+		// student.setEmail("ambrozy@gmail.com");
+		// System.out.println(student.getId());
+		// System.out.println(student.getName());
+		// System.out.println(student.getEmail());
 	}
 
 }
---
+++ src/main/java/pl/gocbar/gallery/entity/Post.java
@@ -0,0 +1,91 @@
+package pl.gocbar.gallery.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    private String url;
+    @Column(nullable = false)
+    private String content;
+    private String shortDescription;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+
+    public Post() {
+    }
+
+    
+    public Post(Long id, String title, String url, String content, String shortDescription, LocalDateTime createdOn,
+            LocalDateTime updatedOn) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+        this.content = content;
+        this.shortDescription = shortDescription;
+        this.createdOn = createdOn;
+        this.updatedOn = updatedOn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public String getShortDescription() {
+        return shortDescription;
+    }
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+    public LocalDateTime getUpdatedOn() {
+        return updatedOn;
+    }
+    public void setUpdatedOn(LocalDateTime updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+
+}
